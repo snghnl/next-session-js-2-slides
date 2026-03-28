ac9fa36c916d9b4f1d41363f3a307fc22910c520
snghnl <viselacity@gmail.com>
2026-03-28 13:38:29 +0900

    add footer

---
+++ global-bottom.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer
+        class="absolute bottom-4 left-0 right-0 flex justify-between items-center px-8 text-xs text-gray-400"
+    >
+        <img src="./assets/next-logo.png" class="h-2" />
+        <span>{{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}</span>
+    </footer>
+</template>
